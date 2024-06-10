75e7544d9485c57a8076fed02ab79c295977faa5
goldJeong <manage06@naver.com>
2024-06-10 21:39:27 +0900

    [Silver IV] Title: 제로, Time: 212 ms, Memory: 17324 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+function calculateSum(k, numbers) {
+    let stack = [];
+
+    for (let i = 0; i < k; i++) {
+        let num = numbers[i];
+        if (num === 0) {
+            stack.pop();
+        } else {
+            stack.push(num);
+        }
+    }
+
+    let sum = stack.reduce((acc, cur) => acc + cur, 0);
+    return sum;
+}
+
+const k = parseInt(input[0], 10);
+const numbers = input.slice(1, k + 1).map(Number);
+
+console.log(calculateSum(k, numbers));
